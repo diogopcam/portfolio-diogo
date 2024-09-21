427823db6135e3a6218c0b029effb30562a8971e
Diogo <diogpcam@gmail.com>
2024-09-21 17:07:29 -0300

    Mais uma tentativa de deploy.

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css'; // Importando o arquivo CSS principal
+import App from './App';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App/>
-  </StrictMode>,
-)
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>   
+
+);
