570b982d8ae16d38b76f069062e56c3a7ba6d16c
Diogo <diogpcam@gmail.com>
2024-09-21 17:50:30 -0300

    Fonte preta.

--- src/screens/LandingPage.jsx
+++ src/screens/LandingPage.jsx
@@ -10,7 +10,7 @@ function LandingPage() {
     animate={{opacity: 1}}
     exit={{opacity: 0}}
     >
-    <div className="bg-[url('/Users/dpcam/Desktop/portfolio-diogo/src/assets/images/360_F_227215665_8HbqJ2T9djgJU4mkLJJEmesp5XYhlUTk.jpg')] bg-cover text-white flex flex-col justify-between h-screen p-4">
+    <div className="bg-[url('/Users/dpcam/Desktop/portfolio-diogo/src/assets/images/360_F_227215665_8HbqJ2T9djgJU4mkLJJEmesp5XYhlUTk.jpg')] bg-cover text-black flex flex-col justify-between h-screen p-4">
         <HeaderComponent/>
         <p className='text-9xl pb-10 pl-10'>
           PORTFÓLIO
