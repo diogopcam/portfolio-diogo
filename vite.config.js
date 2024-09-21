21a1e0a65dfb598b6e73e77e4d132c2e5664d278
Diogo <diogpcam@gmail.com>
2024-09-21 17:03:24 -0300

    Tentativa de novo deploy.

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],   
 
-  base: '/my-app', // Se você estiver usando um caminho base customizado
   build: {
-    outDir: 'build', // Diretório de saída do build
+    outDir: 'dist', // Diretório de saída do build
   },
 });
