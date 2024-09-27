aa8adeb0292a5b97965a88ef2b722ff293ce7ccf
Diogo <diogpcam@gmail.com>
2024-09-26 21:45:01 -0300

    Inicio da implementacao doo registros profissionais e academicos.

--- src/screens/HomePage.jsx
+++ src/screens/HomePage.jsx
@@ -5,39 +5,12 @@ import ProjectFrameComponent from '../components/ProjectFrameComponent';
 import MrImage from '../assets/images/myrecords.png'; // Caminho relativo à imagem local
 import PodioImage from '../assets/images/podio.png'; // Caminho relativo à imagem local
 import TechIcons from '../components/TechIcons';
-
-import { motion } from 'framer-motion';
+// import Thoughtworks from '../assets/trajectory-icons/thoughtworks.png';
+import semBarreirasImage from '../assets/trajectory-icons/sem-barreiras.png'; // Importando a imagem diretamente
+// import thoughtworksImage from '../assets/trajectory-icons/tw.png';
+// import { motion } from 'framer-motion';
 
 function HomePage() {
-//   const navigate = useNavigate();
-
-//   // Função para detectar rolagem
-//   useEffect(() => {
-//     const handleScroll = () => {
-//       // Detecta a posição do scroll atual + altura da janela visível
-//       const scrollPosition = window.scrollY + window.innerHeight;
-//       // Altura total do documento (página completa)
-//       const documentHeight = document.documentElement.scrollHeight;
-
-//       // Verifica se o usuário rolou até próximo do final (tolerância de 50px)
-//       if (scrollPosition >= documentHeight - 50) {
-//         // Navegar para a ProjectsPage com um atraso para permitir animação de saída
-//         setTimeout(() => {
-//           navigate('/projects-page');
-//         }, 500);
-//       }
-//     };
-
-//     // Adiciona o evento de rolagem quando o componente é montado
-//     window.addEventListener('scroll', handleScroll);
-
-//     // Remove o listener de rolagem ao desmontar o componente
-//     return () => {
-//       window.removeEventListener('scroll', handleScroll);
-//     };
-//   }, [navigate]);
-
-  // Dados do projeto
   const myrecords = {
     name: 'My Records',
     description: 'Uma tentativa de replicar o Topsters 3, uma plataforma que permite que os usuários criem colagens de seus álbuns favoritos e realizem o download dessa colagem em pdf.',
@@ -58,6 +31,18 @@ function HomePage() {
     image: PodioImage
   };
 
+  const sembarreiras = {
+    image: semBarreirasImage,
+    name: 'Sem Barreiras',
+    position: 'Desenvolvedor front-end - 03/2023 até 06/2023'
+  };
+
+  // const thoughtworksImage = {
+  //   image: thoughtworksImage,
+  //   name: 'Thoughtworks',
+  //   position: 'Jovem Aprendiz na Thoughtworks - 06/2023 até 08/2024'
+  // };
+
   return (
     // Full page
     <div className="bg-[url('https://i.imgur.com/J0sCj2O.jpg')] bg-cover flex flex-col pl-5 pr-5 items-center">
@@ -69,7 +54,7 @@ function HomePage() {
         {/* Landing Page */}
         <div className='pt-[12vh] h-[100vh] text-white text-center items-center justify-center flex flex-col gap-14'>
             <p className="w-[80vh] text-xl text font-thin"> 
-                Estudante do 4º semestre de Engenharia de Software na PUCRS, sou um aspirante a desenvolvedor full-stack com experiência prática em React, React Native e Java. Busco minha primeira experiência profissional como desenvolvedor para aprimorar minhas habilidades e criar soluções criativas e escaláveis.
+                Estudante do 4º semestre de Engenharia de Software na PUCRS, sou um aspirante a desenvolvedor full-stack com experiência prática em React, React Native e Java. Busco minha primeira experiência profissional como desenvolvedor para aprimorar minhas habilidades e criar soluções eficientes, criativas e escaláveis.
             </p>
             <p className='text-8xl pb-10 pl-10'>
                 DIOGO PESSIN CAMARGO
@@ -80,11 +65,14 @@ function HomePage() {
         <div className='text-white text-center items-center'>
           <p className='text-6xl'> TRAJETÓRIA ACADÊMICA E PROFISSIONAL </p>
           <div className='flex flex-row justify-center gap-40'>
-            <div className='flex flex-col'> 
-              <p> Texto 1 </p>
-              <p> Texto 2 </p>
+            <div className='flex flex-row items-center justify-center border border-gray-300 p-4 shadow-md'>
+              <img src={semBarreirasImage} alt="Sem Barreiras" className="w-24 h-24 bg-white rounded-full" /> {/* Renderiza a imagem aqui */}
+            <div className='pl-4 flex text-left flex-col'>
+              <p> {sembarreiras.name} </p>
+              <p> {sembarreiras.position} </p>
+            </div>  
             </div>
-            <div>
+            <div className='flex flex-col items-center'>
               <p> Texto 2 </p>
               <p> Texto 2 </p>
             </div>
